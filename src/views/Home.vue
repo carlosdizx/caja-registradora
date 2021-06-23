bacd0c3b57c3371cf43fd2c1fa0fd22e600d3c20
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-23 12:46:51 -0500

    Se creo el componente de login, con validaciones y assets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,22 @@
 <template>
+	<Login />
+	<!--
 	<v-card>
 		<ToolBar :tabs="items" color="indigo" titulo="Registrar" />
 	</v-card>
+	-->
 </template>
 
 <script lang="ts">
 	import Vue from 'vue';
-	import ToolBar from '../components/general/ToolBar.vue';
+	import Login from '../components/Login.vue';
 	export default Vue.extend({
 		name: 'Home',
 		data: () => ({
 			items: ['error 500 en frontend'],
 		}),
 		components: {
-			ToolBar,
+			Login,
 		},
 		mounted() {
 			this.items = ['Ventas', 'Personas', 'Productos'];
