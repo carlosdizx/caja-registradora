179ef05d3e0c8db57e74ad7caeb20a0757d7b6c8
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-23 12:02:45 -0500

    Creacion de un toolbar dinamico

---
+++ src/components/general/ToolBar.vue
@@ -0,0 +1,54 @@
+<template>
+	<v-card>
+		<v-toolbar :color="color" dark flat>
+			<v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+			<v-toolbar-title>{{ titulo }}</v-toolbar-title>
+
+			<v-spacer></v-spacer>
+
+			<v-btn icon>
+				<v-icon>mdi-magnify</v-icon>
+			</v-btn>
+
+			<v-btn icon>
+				<v-icon>mdi-dots-vertical</v-icon>
+			</v-btn>
+
+			<template v-slot:extension>
+				<v-tabs v-model="tab" align-with-title>
+					<v-tabs-slider color="yellow"></v-tabs-slider>
+
+					<v-tab v-for="item in items" :key="item">
+						{{ item }}
+					</v-tab>
+				</v-tabs>
+			</template>
+		</v-toolbar>
+
+		<v-tabs-items v-model="tab">
+			<v-tab-item v-for="item in tabs" :key="item">
+				<v-card flat>
+					<v-card-text v-text="text"></v-card-text>
+				</v-card>
+			</v-tab-item>
+		</v-tabs-items>
+	</v-card>
+</template>
+
+<script>
+	export default {
+		name: 'ToolBar',
+		data: () => ({
+			tab: null,
+			text: 'lo que sea',
+		}),
+		props: {
+			tabs: Array,
+			color: String,
+			titulo: String,
+		},
+	};
+</script>
+
+<style scoped></style>
