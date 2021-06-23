903a1044961a92a3a5f7fdf3c9a8abe9c1e99dae
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-23 12:24:48 -0500

    Probando el toolbar dinamico

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
+	<v-card>
+		<ToolBar :tabs="items" color="indigo" titulo="Registrar" />
+	</v-card>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  export default Vue.extend({
-    name: 'Home',
-
-    components: {
-    },
-  })
+	import Vue from 'vue';
+	import ToolBar from '../components/general/ToolBar.vue';
+	export default Vue.extend({
+		name: 'Home',
+		data: () => ({
+			items: ['error 500 en frontend'],
+		}),
+		components: {
+			ToolBar,
+		},
+		mounted() {
+			this.items = ['Ventas', 'Personas', 'Productos'];
+		},
+	});
 </script>
