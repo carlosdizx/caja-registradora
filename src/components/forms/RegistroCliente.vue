ebcdb75549c3a39110400e35d3409fb2b896c895
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-24 16:45:35 -0500

    Desacoplamiento de la vista distintos componentes, ahora en el toolbar de  registro la navegacion se realiza por las tabs estas son dinamicas y escalables

---
+++ src/components/forms/RegistroCliente.vue
@@ -0,0 +1,61 @@
+<template>
+	<v-card>
+		<v-card-title>
+			<h1>Registro cliente</h1>
+		</v-card-title>
+		<v-card-text>
+			<v-form @submit.prevent="submit">
+				<v-text-field type="number" label="documento de identidad" />
+			</v-form>
+		</v-card-text>
+	</v-card>
+</template>
+
+<script>
+	import { required, digits, email, max, min } from 'vee-validate/dist/rules';
+	import {
+		extend,
+		ValidationObserver,
+		ValidationProvider,
+		setInteractionMode,
+	} from 'vee-validate';
+	import { mapActions, mapState } from 'vuex';
+	setInteractionMode('eager');
+
+	extend('digits', {
+		...digits,
+		message: '{_field_} Se necesita {length} digitos. ({_value_})',
+	});
+
+	extend('required', {
+		...required,
+		message: '{_field_} no puede estar vacio',
+	});
+
+	extend('max', {
+		...max,
+		message: '{_field_} {length} maximo de caracteres',
+	});
+
+	extend('min', {
+		...min,
+		message: '{_field_} => Ingrese mas caracteres, minimo {length} ',
+	});
+
+	extend('email', {
+		...email,
+		message: 'Correo con formato incorrecto',
+	});
+	export default {
+		name: 'RegistroCliente',
+		components: {
+			ValidationProvider,
+			ValidationObserver,
+		},
+		methods: {
+			submit() {},
+		},
+	};
+</script>
+
+<style scoped></style>
--- src/views/RegistroVPP.vue
+++ src/views/RegistroVPP.vue
@@ -4,14 +4,15 @@
 
 <script>
 	import ToolBarIcons from '../components/general/ToolBarIcons';
+	import RegistroCliente from '../components/forms/RegistroCliente';
 	export default {
 		name: 'RegistroVPP',
-		components: { ToolBarIcons },
+		components: { ToolBarIcons, RegistroCliente },
 		data: () => ({
 			tabs: [''],
 		}),
 		mounted() {
-			this.tabs = ['mdi-head-plus-outline','mdi-food','mdi-point-of-sale'];
+			this.tabs = ['mdi-head-plus-outline', 'mdi-food', 'mdi-point-of-sale'];
 		},
 	};
 </script>
