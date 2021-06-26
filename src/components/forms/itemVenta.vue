6bdc010f5bb686072de083bd537cce5d18d2cfbc
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-26 12:20:00 -0500

    Creado el componente que maneja una factura para item (producto) de compra de la venta

---
+++ src/components/forms/itemVenta.vue
@@ -0,0 +1,31 @@
+<template>
+	<v-card>
+		{{ producto }}
+		<v-container>
+			<v-select
+				v-model="producto"
+				label="Seleccione producto"
+				:items="items"
+				item-text="nombre"
+			/>
+			<v-text-field v-model.number="cantidad" type="number" label="cantidad" />
+			<v-text-field v-model.number="subtotal" disabled type="number" label="Subtotal" />
+		</v-container>
+	</v-card>
+</template>
+
+<script>
+	export default {
+		name: 'itemVenta',
+		data: () => ({
+			producto: null,
+			cantidad: 1,
+			subtotal: 0,
+		}),
+		props: {
+			items: [],
+		},
+	};
+</script>
+
+<style scoped></style>
