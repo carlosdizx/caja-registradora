d1fd144e177717c9bab55245ec31b2ec7df32e01
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-29 22:44:42 -0500

    Se logro hacer un grafico dinamico con colores que cambian dependiendo del dato

---
+++ src/components/general/GraficoBarras.vue
@@ -0,0 +1,32 @@
+<script>
+	import { Bar } from 'vue-chartjs';
+	export default {
+		extends: Bar,
+		name: 'GraficoBarras',
+		data: () => ({
+			dataSets: [],
+		}),
+		props: {
+			etiquetas: [],
+			colores: [],
+			datos: [],
+		},
+		methods: {
+			async crartChart() {
+				await this.etiquetas.forEach((item, index) => {
+					this.dataSets.push({
+						label: item,
+						backgroundColor: this.colores[index],
+						data: [this.datos[index]],
+					});
+				});
+				this.renderChart({
+					datasets: this.dataSets,
+				});
+			},
+		},
+		mounted() {
+			this.crartChart();
+		},
+	};
+</script>
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,22 +1,55 @@
 <template>
-  <div>
-    <ToolBar titulo="Inicio" color="indigo" :tabs="tabs" />
-    <ResumenApp />
-    <Grafico />
-  </div>
+	<div>
+		<ToolBar titulo="Inicio" color="indigo" :tabs="tabs" />
+		<ResumenApp />
+		<Grafico :etiquetas="etiquetas" :datos="datos" :colores="colores" />
+	</div>
 </template>
 
 <script>
 	import ToolBar from '../components/Inicio/ToolBarInicio';
-	import ResumenApp from "../components/Inicio/ResumenApp";
-  import Grafico from "../components/general/Grafico";
+	import ResumenApp from '../components/Inicio/ResumenApp';
+	import Grafico from '../components/general/GraficoBarras';
 	import { ITEMS } from '../assets/itemsInicio';
 	export default {
 		name: 'Inicio',
 		components: {
-			ToolBar,ResumenApp,Grafico
+			ToolBar,
+			ResumenApp,
+			Grafico,
 		},
-		data: () => ({ tabs: [{ name: '', link: '' }] }),
+		data: () => ({
+			tabs: [{ name: '', link: '' }],
+			etiquetas: [
+				'Enero',
+				'Febrero',
+				'Marzo',
+				'Abril',
+				'Mayo',
+				'Junio',
+				'Julio',
+				'Agosto',
+				'Septiembre',
+				'Octubre',
+				'Noviembre',
+				'Diciembre',
+			],
+			datos: [10, 20, 10, 11, 5, 2, 3, 5, 6, 10, 20, 50],
+			colores: [
+				'green',
+				'blue',
+				'red',
+				'yellow',
+				'pink',
+				'cyan',
+				'indigo',
+				'orange',
+				'purple',
+				'brown',
+				'olive',
+				'dark',
+			],
+		}),
 		mounted() {
 			this.tabs = ITEMS;
 		},
